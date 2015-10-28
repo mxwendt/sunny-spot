2e49fa0a2d65c57546ee2ad5a1252a84270bc693
Max Wendt <mxwendt@gmail.com>
2015-10-28 12:20:05 +0100

    Updated the GPS example for the Rotundan restaurant

--- app.js
+++ app.js
@@ -1,101 +1,138 @@
+// Retrieve the immersive context
 var context = Argon.immersiveContext;
-var options = THREE.Bootstrap.createArgonOptions( context );
-var three = THREE.Bootstrap( options );
+
+// Initialize Three.js rendering
+var options = THREE.Bootstrap.createArgonOptions(context);
+var three = THREE.Bootstrap(options);
 
 /**
-  Buzz above the Turning Torso
-*/
+ * All geospatial objects need to have an Object3D liked to a Cesium Entity. We
+ * need to do this because Argon needs a mapping between Entities and Object3Ds.
+ *
+ * There are two ways to do this:
+ * 1. Place an object using a known LLA.
+ * 2. Place an object near the starting location.
+ */
 
-var buzz = new THREE.Object3D;
-var loader = new THREE.TextureLoader();
-loader.load( 'buzz.png', function ( texture ) {
+// Place a cube above the Restaurant Rotundan
+
+// Create a new Object3D
+var rotundan = new THREE.Object3D;
+
+// Add a texture to the Object3D
+var rotundanTextureLoader = new THREE.TextureLoader();
+rotundanTextureLoader.load('buzz.png', function (texture) {
   var geometry = new THREE.BoxGeometry(10, 10, 10);
   var material = new THREE.MeshBasicMaterial({ map: texture });
-  var mesh = new THREE.Mesh( geometry, material );
+  var mesh = new THREE.Mesh(geometry, material);
   mesh.scale.set(10, 10, 10);
-  buzz.add( mesh );
+  rotundan.add(mesh);
 });
 
-var turningTorsoGeo = new Argon.Cesium.Entity({
-  name: "Turning Torso",
-  position: Argon.Cesium.Cartesian3.fromDegrees(55.6133068, 12.9741071, 300)
+// Create a new Cesium Entity
+var rotundanGeoEntity = new Argon.Cesium.Entity({
+  name: 'Restaurant Rotundan',
+  position: Argon.Cesium.Cartesian3.fromDegrees(55.587422, 13.053773)
 });
 
-var turningTorsoGeoTarget = three.argon.objectFromEntity(turningTorsoGeo);
-turningTorsoGeoTarget.add(buzz);
+var rotundanGeoTarget = three.argon.objectFromEntity(rotundanGeoEntity);
+rotundanGeoTarget.add(rotundan);
 
 /**
- The wooden box in front of me
-*/
+ * Each time our context is assigned a new Reality, including the first time, we
+ * receive a "realityChange" event. We should do initialization that is based on
+ * the state of the world here.
+ *
+ * A state parameter looks like this:
+ * {
+ *   reality: Argon.Reality,
+ *   previousReality; Argon.Reality
+ * }
+ */
 
-var boxGeoObject = new THREE.Object3D;
-var box = new THREE.Object3D;
-var loader = new THREE.TextureLoader();
-loader.load( 'box.png', function ( texture ) {
- var geometry = new THREE.BoxGeometry(1, 1, 1);
- var material = new THREE.MeshBasicMaterial( { map: texture } );
- var mesh = new THREE.Mesh( geometry, material );
- box.add( mesh );
-});
-
-boxGeoObject.add(box);
-three.scene.add(boxGeoObject );
-
-var boxGeoEntity = three.argon.entityFromObject(boxGeoObject);
-
+// This will tell us if we have a reality
 var realityInit = false;
-var boxPos= [0, 0, 0];
-three.on("argon:realityChange", function(event) {
+
+// Listen for reality changes
+three.on('argon:realityChange', function(e) {
   realityInit = true;
-  // set the position to be near the camera
-  var cameraPosition = three.camera.getWorldPosition();
-  cameraPosition.x += 5;
-  boxGeoObject.position.copy(cameraPosition);
-  three.argon.updateEntityFromObject(boxGeoObject);
-  // getCartographicDegreesFromEntity will return undefined if geoObject is not defined in geographic coordinates  which would happen if this Reality does not support geographic coordinates
-  geoObjectPos = three.argon.getCartographicDegreesFromEntity(boxGeoObject) || [0,0,0];
 });
 
-var lastInfoText;
+/**
+ * Argon's update state is stored in the threestrap update event in the
+ * argonState property. In general, this is only needed for lower-level
+ * information from Argon, such as the raw geolocation.
+ *
+ * An Argon state parameter looks like this:
+ * {
+ *   frameNumber: number (int),
+ *   time: a Cesium time for the update,
+ *   referenceFrame: Cesium.ReferenceFrame.FIXED or {id: frameID} // the root reference
+ *   position: {
+ *     cartesian: Cesium.Cartesian,
+ *     cartographicDegrees: [longitude, latitude, height]
+ *   },
+ *   orientation: {
+ *     unitQuaternion: Cesium.Quaternion, // Orientation in reference frame
+ *     unitQuaternionRelative: Cesium.Quaternion, // Orientation relative to local origin
+ *   },
+ *   frustum: {
+ *     fov: number,
+ *     fovy: number,
+ *     aspectRatio: number
+ *   },
+ *   reality: {
+ *     id: realityID
+ *   }
+ * }
+ */
 
-three.on('update', function(event) {
-  var elem = document.getElementById('location');
-  var state = event.argonState;
+// This will save the last info text that was rendered
+var lastInfoText = '';
 
-  // Ignore updates until we have a reality
+three.on('update', function(e) {
+  var locationElem = document.getElementById('location');
+  var state = e.argonState;
+  var gpsCartographicDeg = [0, 0, 0];
+  var rotundanGeographicDeg = [0, 0, 0];
+  var cameraPos;
+  var rotundanPos;
+  var distanceToRotundan;
+  var infoText;
+
+  // Ignore update until we have a reality
   if (! realityInit) {
-    elem.innerText = "No Reality Yet";
+    locationElem.innerText = 'No reality yet';
     return;
   }
 
-  // Get the position of the device
-  var gpsPos = [0, 0, 0];
+  // Get the Argon's cartographic degrees [longitude, latitude, height]
   if (state.position.cartographicDegrees) {
-    gpsPos = state.position.cartographicDegrees;
+    gpsCartographicDeg = state.position.cartographicDegrees;
   }
 
-  // Rotate both boxes for some visual sugar
-  buzz.rotation.y += 2 * three.Time.delta;
-  box.rotation.y += 3 * three.Time.delta;
-
-  // Compute distance from the camera to each of the two cubes
-  var point1 = three.camera.getWorldPosition();
-  var point2 = buzz.getWorldPosition();
-  var point3 = box.getWorldPosition();
-  var distance = point1.distanceTo( point2 );
-  var distance2 = point1.distanceTo( point3 );
-
-  // Output some information
-  var infoText = "Geospatial Argon3 example:\n"
-  infoText += "eye (" + toFixed(gpsPos[0],6) + ", ";
-  infoText += toFixed(gpsPos[1], 6) + ", " + toFixed(gpsPos[2], 2) + ")\n";
-  infoText += "cube(" + toFixed(boxPos[0], 6) + ", ";
-  infoText += toFixed(boxPos[1], 6) + ", " + toFixed(boxPos[1],2) + ")\n";
-  infoText += "distance to GT (" + toFixed(distance,2) + ")";
-  infoText += " distance to box (" + toFixed(distance2,2) + ")";
-
-  if (lastInfoText !== infoText) {
-    elem.innerText = infoText;
-    lastInfoText = infoText;
+  // Get Rotundans cartographic degrees as well
+  if (three.argon.getCartographicDegreesFromEntity(rotundanGeoEntity)) {
+    rotundanGeographicDeg = three.argon.getCartographicDegreesFromEntity(rotundanGeoEntity)
   }
+
+  // Calculate some information
+  cameraPos = three.camera.getWorldPosition();
+  rotundanPos = rotundan.getWorldPosition();
+  distanceToRotundan = cameraPos.distanceTo(rotundanPos);
+
+  // Ouput some information
+  infoText = 'Operation Rotundan:\n';
+  infoText += 'frame: ' + state.frameNumber;
+  infoText += 'argon time: (' + three.argon.time.secondsOfDay + ')\n';
+  infoText += 'three time: (' + three.Time.now + ')\n';
+  infoText += 'camera: ' + cameraPos + '\n';
+  infoText += 'eye (' + gpsCartographicDeg[0] + ', ' + gpsCartographicDeg[1] + ', ' + gpsCartographicDeg[2] + '\n';
+  infoText += 'rotundan (' + rotundanGeographicDeg[0] + ', ' + rotundanGeographicDeg[1] + ', ' + rotundanGeographicDeg[2] + '\n';
+  infoText += 'distance to rotundan (' + distanceToRotundan + ')';
+
+  // if (lastInfoText !== infoText) {
+    locationElem.innerText = infoText;
+    // lastInfoText = infoText;
+  // }
 });
